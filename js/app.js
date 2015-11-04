6e175ecf8640844739513c0829501d8b7a9e1d49
kpfein <kpfein@yahoo.com>
2015-11-03 22:07:08 -0600

    w2d5

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	$('#tweet-content .tweet-compose').on('click', function(){
 		$(this).css("height", "5em");
 		$('#tweet-controls #tweet-submit, #tweet-controls #char-count').css("display", "inline-block");
@@ -9,7 +10,7 @@ $(document).ready(function(){
 		var length = $(this).val().length;
 		var length = max - length;
 		$("#char-count").text(length);
-		if (length <= 10 && (length > 0)){
+		if ((length <= 10) && (length > 0)){
 			$("#char-count").css("color", "red");
 			$('#tweet-controls #tweet-submit').prop('disabled', false);
 		} else if (length < 0) {
